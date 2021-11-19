6823637fe2ac6a8d2fc79bf5414cb3db5a6404a4
kidotedla <tedlakidus0@gmail.com>
2021-11-18 18:50:10 -0800

    0x02 import mod

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    op = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(op.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    num1, num2, c = int(sys.argv[1]), int(sys.argv[3]), sys.argv[2]
+
+    print("{} {} {} = {}".format(num1, c, num2, op[c](num1, num2)))
+    
---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+
+
+def printArguments():
+
+    length = len(sys.argv)
+    if (length == 1):
+        print("0 arguments.")
+    elif (length == 2):
+        print("{} argument:".format(length - 1))
+        print("{}: {}".format(1, sys.argv[1]))
+
+    else:
+        print("{} arguments:".format(length - 1))
+        for i in range(1, length):
+            print("{}: {}".format(i, sys.argv[i]))
+
+
+if __name__ == "__main__":
+    printArguments()
+    
